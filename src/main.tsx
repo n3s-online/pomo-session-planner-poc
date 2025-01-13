48009e387217379ffd564faf2cd25d77899bb848
n3s-online <n3s.online@gmail.com>
2025-01-13 11:34:51 -0800

    set up tailwind and stuff

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+    <div className="text-blue-500 font-bold">Tailwind Test</div>
+  </StrictMode>
+);
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { ghPages } from "vite-plugin-gh-pages";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), ghPages()],
   base: "/pomo-session-planner-poc/",
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
